d4722528110c170bac9c032942009a442e80ae45
rasimbot <rasimbot@hotmail.com>
2017-11-23 23:04:03 +0300

    Fix the definition of π

--- src/magic-renderer/defs.h
+++ src/magic-renderer/defs.h
@@ -4,7 +4,7 @@
 namespace Magic
 {
     const float s_piF = 3.1415926535897932384626433832795f;
-    const double s_piD = 3.1415926535897932384626433832795f;
+    const double s_piD = 3.1415926535897932384626433832795;
 }
 
 #endif // __MAGIC_RENDERER_DEFS_H
